387c2cd43171bc2cf51227e8803023d207314373
profLedson <ledson.vanini@escola.pr.gov.br>
2024-09-17 10:47:20 -0300

    Novo commit de teste

---
+++ EscrevendoNaDOM/script.js
@@ -0,0 +1,10 @@
+// SELECIONAR
+const nome = document.getElementById('nome');
+const sobrenome = document.getElementById('sobrenome');
+
+// MANIPULAR
+const novoNome = "Brunão"
+const novoSobrenome = "Carrara"
+
+nome.innerText = novoNome;
+sobrenome.innerText = novoSobrenome3
